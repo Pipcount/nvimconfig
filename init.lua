6aacc63ba1f686f040a4e74d8f873e10ffadfee5
Pipcount <nico@janke-online.net>
2024-04-20 18:45:03 +0200

    added open scene builder

--- init.lua
+++ init.lua
@@ -811,3 +811,25 @@ require('lazy').setup({
 --
 -- Custom Keybinds
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Open FXML file with Scene Builder
+vim.api.nvim_set_keymap('n', '<leader>sb', ':lua OpenSceneBuilder()<CR>', { noremap = true, silent = true })
+
+function OpenSceneBuilder()
+  -- Get the path of the currently edited file
+  local file_path = vim.fn.expand '%:p'
+
+  -- Check if the file has .fxml extension
+  if string.match(file_path, '%.fxml$') then
+    -- Open the file with Scene Builder
+    local scenebuilder_path = '~/Apps/scenebuilder/bin/SceneBuilder'
+    vim.fn.system(scenebuilder_path .. ' ' .. file_path)
+  else
+    print 'Not an FXML file'
+  end
+end
