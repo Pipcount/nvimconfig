7707a7d9921c2156d8fb17a030eb188d53a18102
Pipcount <nico@janke-online.net>
2024-10-28 16:10:18 +0100

    changed colorscheme

--- init.lua
+++ init.lua
@@ -690,13 +690,13 @@ require('lazy').setup({
         -- change the command in the config to whatever the name of that colorscheme is.
         --
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'folke/tokyonight.nvim',
+        'navarasu/onedark.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
         init = function()
             -- Load the colorscheme here.
             -- Like many other themes, this one has different styles, and you could load
             -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
+            vim.cmd.colorscheme 'onedark'
 
             -- You can configure highlights by doing something like:
             vim.cmd.hi 'Comment gui=none'
@@ -843,9 +843,3 @@ function OpenSceneBuilder()
         print 'Not an FXML file'
     end
 end
-
--- tabwidth
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 0
-vim.opt.expandtab = false
-vim.opt.shiftwidth = 4
